30b9e73f674bd413d683350acafd110b4535d928
ox0114xo <ox0114xo@gmail.com>
2026-02-21 19:15:54 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-const CACHE_NAME = 'WinnerFlow-v6';
+const CACHE_NAME = 'WinnerFlow-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(
     keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
   )));
 });
+
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
